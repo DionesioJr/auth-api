1d22c057e783e9bd68d1ef50c9d914cdd6d65b48
Dionésio Guerra <dionesiojr@gmail.com>
2025-01-26 12:46:17 -0300

    :wrench: config

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,37 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  // Carregar o arquivo .env
+  config();
+
   const app = await NestFactory.create(AppModule);
 
   // Habilitar transformação e validação global
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true, // Transforma as strings em objetos Date automaticamente
-      whitelist: true, // Remove propriedades não especificadas no DTO
-      forbidNonWhitelisted: true, // Gera erro para propriedades não especificadas
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
 
-  const config = new DocumentBuilder()
+  // Configurar Swagger
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Auth API')
     .setDescription('API for users')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 8000);
+  // Usar a porta definida no .env ou a porta padrão 8000
+  const port = process.env.APP_PORT || 3000;
+  console.log(`Server running on port ${port}`);
+  await app.listen(port);
 }
-bootstrap();
+
+void bootstrap();
