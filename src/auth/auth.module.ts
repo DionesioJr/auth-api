e72c7a5028f3e8cfc649c0cf51c9da09111ef95a
Dionésio Guerra <dionesiojr@gmail.com>
2025-01-27 14:57:52 -0300

    :sparkles: feat: Melhorando modulo auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from 'src/database/prisma.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { LoginUseCase } from './usecases/login.usecase';
 import { LogoutUseCase } from './usecases/logout.usecase';
 import { RefreshTokenUseCase } from './usecases/refresh-token.usecase';
@@ -15,6 +16,6 @@ import { ValidateTokenUseCase } from './usecases/validate-token.usecase';
     }),
   ],
   controllers: [AuthController],
-  providers: [PrismaService, LoginUseCase, LogoutUseCase, RefreshTokenUseCase, ValidateTokenUseCase],
+  providers: [PrismaService, AuthService, LoginUseCase, LogoutUseCase, RefreshTokenUseCase, ValidateTokenUseCase],
 })
 export class AuthModule {}
