0ff407c6b8ddac0c70030f70251dfa9f4770565b
Dionésio Guerra <dionesiojr@gmail.com>
2025-01-28 17:22:37 -0300

    :sparkles: feat: Melhorando modulo auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,15 +7,19 @@ import { LoginUseCase } from './usecases/login.usecase';
 import { LogoutUseCase } from './usecases/logout.usecase';
 import { RefreshTokenUseCase } from './usecases/refresh-token.usecase';
 import { ValidateTokenUseCase } from './usecases/validate-token.usecase';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     JwtModule.register({
+      global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: '60s' },
     }),
   ],
   controllers: [AuthController],
   providers: [PrismaService, AuthService, LoginUseCase, LogoutUseCase, RefreshTokenUseCase, ValidateTokenUseCase],
+  exports: [AuthService],
 })
 export class AuthModule {}
