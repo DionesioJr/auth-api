2518901135c5d921bdb6adc9686b49e74e9f3a55
Dionésio Guerra <dionesiojr@gmail.com>
2025-01-28 17:21:59 -0300

    :sparkles: feat: Melhorando modulo auth

--- src/tenants/usecases/validate-tenant-subdomain.usecase.ts
+++ src/tenants/usecases/validate-tenant-subdomain.usecase.ts
@@ -1,5 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
+import { ReservedSubdomains } from '../reserved-subdomains';
 
 @Injectable()
 export class ValidateTenantSubdomainUseCase {
@@ -10,10 +15,25 @@ export class ValidateTenantSubdomainUseCase {
   async execute(subdomain: string): Promise<{ isAvailable: boolean }> {
     this.logger.log(`Validating subdomain: ${subdomain}.`);
 
-    const existingTenant = await this.prisma.tenants.findUnique({
-      where: { subdomain },
-    });
+    try {
+      // Verifica se o subdomínio está na lista de reservados
+      if (ReservedSubdomains.includes(subdomain)) {
+        this.logger.warn(`Subdomain '${subdomain}' is reserved.`);
+        return { isAvailable: false };
+      }
 
-    return { isAvailable: !existingTenant };
+      // Verifica se o subdomínio já existe no banco de dados
+      const existingTenant = await this.prisma.tenants.findUnique({
+        where: { subdomain },
+      });
+
+      // Retorna a disponibilidade do subdomínio
+      const isAvailable = !existingTenant;
+      this.logger.log(`Subdomain '${subdomain}' is ${isAvailable ? 'available' : 'not available'}.`);
+      return { isAvailable };
+    } catch (error) {
+      this.logger.error(`Error while validating subdomain: ${error.message}`, error.stack);
+      throw new InternalServerErrorException('Error while validating subdomain');
+    }
   }
 }
