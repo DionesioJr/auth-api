376d05b9d55d66f50a3dec3851f75adf8c1c31c0
Dionésio Guerra <dionesiojr@gmail.com>
2025-01-28 17:21:41 -0300

    :sparkles: feat: Melhorando modulo auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
-import { RefreshTokenDto } from './dto/refresh-token.dto';
-import { ValidateTokenDto } from './dto/validate-token.dto';
 import { LoginUseCase } from './usecases/login.usecase';
 import { LogoutUseCase } from './usecases/logout.usecase';
 import { RefreshTokenUseCase } from './usecases/refresh-token.usecase';
@@ -17,25 +15,18 @@ export class AuthService {
   ) {}
 
   async login(loginDto: LoginDto, headers: Record<string, string>) {
-    return this.loginUseCase.execute(loginDto, {
-      'device-name': headers['device-name'],
-      'user-agent': headers['user-agent'],
-      ip: headers['x-forwarded-for'] || headers['remote-addr'] || 'Unknown',
-    });
+    return this.loginUseCase.execute(loginDto, headers);
   }
 
   async logout(userId: number) {
     return this.logoutUseCase.execute(userId);
   }
 
-  async refresh(refreshTokenDto: RefreshTokenDto, headers: Record<string, string>) {
-    return this.refreshTokenUseCase.execute(refreshTokenDto, {
-      ip: headers['x-forwarded-for'] || headers['remote-addr'] || 'Unknown',
-      'user-agent': headers['user-agent'],
-    });
+  async refresh(headers: Record<string, string>) {
+    return this.refreshTokenUseCase.execute(headers);
   }
 
-  async validate(validateTokenDto: ValidateTokenDto) {
-    return this.validateTokenUseCase.execute(validateTokenDto);
+  async validate(headers: Record<string, string>) {
+    return this.validateTokenUseCase.execute(headers);
   }
 }
