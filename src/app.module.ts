d2fd47426a256837e509f6378c675e50c561b347
Dionésio Guerra <dionesiojr@gmail.com>
2025-02-11 20:39:53 -0300

    :fire: chore: Removido código obsoleto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
-import { TenantsModule } from './tenants/tenants.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AccountsModule, TenantsModule, AuthModule],
+  imports: [UsersModule, AccountsModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
